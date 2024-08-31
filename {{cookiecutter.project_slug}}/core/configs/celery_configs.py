9756afcbd623a1cf3b130f19c83d03dd1c6a7eab
abolfazl <abolfazlkameli0@gmail.com>
2024-08-31 23:25:29 +0330

    setup/fix: make celery configs better.

--- {{cookiecutter.project_slug}}/core/configs/celery_configs.py
+++ {{cookiecutter.project_slug}}/core/configs/celery_configs.py
@@ -3,10 +3,18 @@ from datetime import timedelta
 from decouple import config
 
 broker_connection_retry_on_startup = True
+
+{ % if cookiecutter.celery_message_broker == 'rabbitmq-server' %}
 broker_url = "pyamqp://guest@localhost//"
 result_backend = 'rpc://'
+{ % elif cookiecutter.celery_message_broker == 'redis' %}
+broker_url = "redis://localhost:6379/0"
+result_backend = "redis://localhost:6379/0"
+{ % endif %}
 worker_prefetch_multiplier = 3
+{ % if cookiecutter.user_timezone %}
 timezone = config('TIME_ZONE')
+{ % endif %}
 task_serializer = 'json'
 result_serializer = 'pickle'
 accept_content = ['json', 'pickle']
