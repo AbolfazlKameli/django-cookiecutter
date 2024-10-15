38a8c0b8e2ec2f8313277f142c20421aa25f43ff
abolfazl <abolfazlkameli0@gmail.com>
2024-10-15 19:57:24 +0330

    feat(hook): add makemigration hool.

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,11 +1,17 @@
 import os
 import subprocess
 
+project_dir = os.path.join(os.getcwd())
+
 
 def run_git_init():
-    project_dir = os.path.join(os.getcwd())
     os.chdir(project_dir)
     subprocess.run(['git', 'init'], check=True)
+
+
+def run_make_migrations():
+    os.chdir(project_dir)
+    subprocess.run(['python', 'manage.py', 'makemigrations'], check=True)
 
 
 if __name__ == "__main__":
