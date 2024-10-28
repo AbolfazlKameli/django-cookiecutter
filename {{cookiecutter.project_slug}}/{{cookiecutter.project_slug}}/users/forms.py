4985d8828dc98c65c12f78ff9b74d3d465fbc1ab
abolfazl <abolfazlkameli0@gmail.com>
2024-10-28 21:47:40 +0330

    fix(forms): remove useless UserRegisterForm.

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/forms.py
@@ -37,34 +37,6 @@ class UserChangeForm(forms.ModelForm):
         fields = ('username', 'email', 'password', 'last_login')
 
 
-class UserRegisterForm(forms.Form):
-    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(label='password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label="confirm password", widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-
-    def clean_username(self):
-        username = self.cleaned_data.get('username')
-        user = User.objects.filter(username=username).exists()
-        if user:
-            raise ValidationError('this username already taken')
-        return username
-
-    def clean_email(self):
-        email = self.cleaned_data.get('email')
-        user = User.objects.filter(email=email).exists()
-        if user:
-            raise ValidationError('this email already in use')
-        return email
-
-    def clean(self):
-        cd = super().clean()
-        password1 = cd.get('password1')
-        password2 = cd.get('password2')
-        if password1 and password2 and password1 != password2:
-            raise ValidationError('Passwords don`t match')
-
-
 class UserLoginForm(forms.Form):
     email = forms.CharField(widget=forms.EmailInput({'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput({'class': 'form-control'}))
